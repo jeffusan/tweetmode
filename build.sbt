f6972b20f494a9a9906429a558b7fbb13684d8d7
Jeff Hemminger <jeff.hemminger@deciphernow.com>
2019-04-23 07:16:17 -0400

    reduced logging to see messages

--- build.sbt
+++ build.sbt
@@ -5,10 +5,10 @@ version := "0.1"
 scalaVersion := "2.11.12"
 
 libraryDependencies ++= Seq(
-  "org.apache.bahir" %% "spark-streaming-twitter" % "2.4.0",
-  "org.apache.spark" %% "spark-tags" % "2.4.0",
+  "org.apache.bahir" %% "spark-streaming-twitter" % "2.3.2",
+  "org.apache.spark" %% "spark-tags" % "2.3.2",
   "org.twitter4j" % "twitter4j-stream" % "4.0.6",
-  "org.apache.spark" %% "spark-streaming" % "2.4.0" % "provided",
+  "org.apache.spark" %% "spark-streaming" % "2.3.2",
   "com.github.pureconfig" %% "pureconfig" % "0.10.2",
   "org.typelevel" %% "cats-core" % "1.6.0",
   "org.typelevel" %% "cats-effect" % "1.2.0",
--- src/main/scala/tweetmode.scala
+++ src/main/scala/tweetmode.scala
@@ -1,31 +1,50 @@
 
 import config.AppConfig
+import org.apache.log4j.{Level, LogManager, Logger}
 import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.ReceiverInputDStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.twitter.TwitterUtils
 import pureconfig.generic.auto._
-import twitter4j.FilterQuery
+import twitter4j.{FilterQuery, Status}
 
 object tweetmode {
 
   case class TweetCreds(consumerKey: String, consumerSecret: String, accessToken: String, accessTokenSecret: String)
-  def main(args: Array[String]): Unit = {
 
+  def streamingContext: StreamingContext = {
+
+    val sparkConf = new SparkConf().setAppName("TwitterPopularTags")
+    if (!sparkConf.contains("spark.master")) {
+      sparkConf.setMaster("local[*]")
+    }
+    new StreamingContext(sparkConf, Seconds(2))
+  }
+
+  def streamFromEnv(ssc: StreamingContext): ReceiverInputDStream[Status] = {
     val config = pureconfig.loadConfigOrThrow[AppConfig]("tweetmode")
 
     System.setProperty("twitter4j.oauth.consumerKey", config.consumerKey)
     System.setProperty("twitter4j.oauth.consumerSecret", config.consumerSecret)
     System.setProperty("twitter4j.oauth.accessToken", config.accessToken)
     System.setProperty("twitter4j.oauth.accessTokenSecret", config.accessTokenSecret)
+    TwitterUtils.createStream(ssc, None)
+  }
 
-    val sparkConf = new SparkConf().setAppName("TwitterPopularTags")
-    if (!sparkConf.contains("spark.master")) {
-      sparkConf.setMaster("local[2]")
-    }
-    val ssc = new StreamingContext(sparkConf, Seconds(2))
-    val filters = args.takeRight(args.length - 4)
+  def main(args: Array[String]): Unit = {
 
-    val stream = TwitterUtils.createStream(ssc, None, filters)
+    LogManager.getRootLogger.setLevel(Level.WARN)
+
+    val ssc = streamingContext
+    val stream = streamFromEnv(ssc)
+
+    processStream(stream)
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  def processStream(stream: ReceiverInputDStream[Status]) = {
     val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
 
     val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
@@ -49,10 +68,6 @@ object tweetmode {
       println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
       topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
     })
-
-    ssc.start()
-    ssc.awaitTermination()
-    println("Hello, world!")
   }
 
 }
